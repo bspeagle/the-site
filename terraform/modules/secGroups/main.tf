3a4346300c0f56f07f4e0fc6c184d69e2ece4354
Brian Speagle <bspeagle@gmail.com>
2019-01-21 21:38:07 -0500

    forgot to remove output for bastion host in secgroups.

--- terraform/modules/secGroups/main.tf
+++ terraform/modules/secGroups/main.tf
@@ -103,6 +103,6 @@ output "ec2sg_id" {
   value = "${aws_security_group.ec2sg.id}"
 }
 
-output "bastionsg_id" {
-  value = "${aws_security_group.bastion_sg.id}"
-}
+// output "bastionsg_id" {
+//   value = "${aws_security_group.bastion_sg.id}"
+// }
